98579ac5fa1bc892cf7392b8d1dec0b66e61a614
Martin Manelli <manelli.ml@gmail.com>
2019-07-16 18:54:37 -0300

    Improve integration test assertions

--- test/lib/sqs/integration_test.exs
+++ test/lib/sqs/integration_test.exs
@@ -13,11 +13,12 @@ defmodule ExAws.SQSIntegrationTest do
   end
 
   test "add_permission/2" do
-    assert {:ok, _} = SQS.add_permission(@queue_name, "TestAddPermission") |> ExAws.request()
+    assert {:ok, %{body: _}} =
+             SQS.add_permission(@queue_name, "TestAddPermission") |> ExAws.request()
   end
 
   test "add_permission/3" do
-    assert {:ok, _} =
+    assert {:ok, %{body: _}} =
              SQS.add_permission(@queue_name, "TestAddPermission2", %{
                "681962096817" => :all,
                "071669896281" => [:send_message, :receive_message]
@@ -33,7 +34,7 @@ defmodule ExAws.SQSIntegrationTest do
 
     receipt_handle = List.first(messages).receipt_handle
 
-    assert {:ok, _} =
+    assert {:ok, %{body: %{request_id: _}}} =
              SQS.change_message_visibility(context.queue_url, receipt_handle, 300)
              |> ExAws.request()
   end
@@ -82,29 +83,35 @@ defmodule ExAws.SQSIntegrationTest do
         }
       end)
 
-    assert {:ok, _} =
+    assert {:ok, %{body: %{request_id: _, successes: successes}}} =
              SQS.change_message_visibility_batch(context.queue_url, messages_payload)
              |> ExAws.request()
+
+    assert successes |> Enum.sort() == Enum.map(messages, & &1.message_id) |> Enum.sort()
   end
 
   test "create_queue/1" do
-    assert {:ok, %{body: %{queue_url: _}}} =
-             SQS.create_queue("yet_another_test_queue") |> ExAws.request()
+    queue_name = "test_queue_2"
+
+    assert {:ok, %{body: %{queue_url: queue_url}}} =
+             SQS.create_queue(queue_name) |> ExAws.request()
+
+    assert queue_url |> String.contains?(queue_name)
   end
 
   test "create_queue/2" do
-    assert {:ok, %{body: %{queue_url: _}}} =
-             SQS.create_queue("another_queue2", receive_message_wait_time_seconds: 20)
+    queue_name = "test_queue_3"
+
+    assert {:ok, %{body: %{queue_url: queue_url}}} =
+             SQS.create_queue(queue_name, receive_message_wait_time_seconds: 20)
              |> ExAws.request()
+
+    assert queue_url |> String.contains?(queue_name)
   end
 
   test "delete_message/2", context do
-    assert {:ok, _} =
-             SQS.delete_message(
-               context.queue_url,
-               "AQEB0aw+z96sMOUWLQuIzA7nPHS7zUOIRlV0OEqvDoKtNcHxSVDQEfY0gBOJKGcnyTvIUncpimPv0CfQDFbwmdU9E00793cP19Bx8BqzuS0sNrARyY4M4xVi7ceVYHMSNU1uyF/+sK6u8yAGnsbsgmPg4AUs5oapv5Qawiq5HJGgH3cmRPy5/IW+b9W6HVy//uNzejbIcAjQX58Dd79D4AGb9Iu4dqfEVK7zo5BCTy+pz9hqGf5MT3jkrd5umjwGdrg3sVBYhrLjmgaqftON8JclkmrUJk0LzPwQ4DdpT8oz5mh7VzAjRXkIA0IQ8PGFFGPMIb8gWNzJ4KA4/OYlnDYyGw=="
-             )
-             |> ExAws.request()
+    assert {:ok, %{body: %{request_id: _}}} =
+             SQS.delete_message(context.queue_url, "a_receipt_handle") |> ExAws.request()
   end
 
   test "delete_message_batch/2", context do
@@ -112,31 +119,29 @@ defmodule ExAws.SQSIntegrationTest do
              SQS.delete_message_batch(context.queue_url, [
                %{
                  id: "message_1",
-                 receipt_handle:
-                   "AQEBlA6/i+8F3P6lA7y3msc8dINnF+b3VgTX71nMWw7VvbHc8mdGFyZjVAVMH/rg6Vyc00O2Tl2ZyKn8IPUiy6n44ipop+xb33XNU/cABvVWZogNN95b9mmR6RuSA0dcVmFL02TwZDpg7cMOWNhYThEp+a5atsG85PX7V6q9zBklltBSQnT6r9QSngnv2m1C23jfFYow0oy86cofp0mQ4z5ez9bWmlHa4XfpZUpP2KVlBCDgyR0tQQRGt170foph32Cg+Bp6RRv9Tyo7aVWqM4OT/CHTJ0ZPiAYoH8MYFxjUaqoeKhUwDFq36trQxrBq9BfBj+hrzEtDQdxcNZM2pZi2xQ=="
+                 receipt_handle: "a_receipt_handle"
                },
                %{
                  id: "message_2",
-                 receipt_handle:
-                   "AQEBc8vuFKIpTF3ESXTpxc2+vARUXHpGzup9YwTMD7Alibe/z/yXEPaXY4ZtTUvInYfEazLhughdoLGSEh1SDPsIdDB9Os8D84xHmtXelswA7FBXEdNunRk4wg6Zi4jgjEy3Kyy9cGpiZwRxw4Vy4PrK7H0BbH07k+mVby8P8B9m97GO/w666/zU46QpFB6jhi7L0d76AW16/PMzEBbDB6zUvXiYUAMmxvdppYrcYqb22K0gWvZsL1Dogr592k/fA1W2oF1YsjTSn9FjYr/q5XK1Z1Lvvmh3/20D5U0qjnFd4wg9MlVp8zrBg2mNoVl6QEHPNP/zA+dZg2d/6SSgEdI1hQ=="
+                 receipt_handle: "another_receipt_handle"
                }
              ])
              |> ExAws.request()
   end
 
   test "delete_queue/1" do
-    {:ok, %{body: %{queue_url: queue_url}}} =
-      SQS.create_queue("another_test_queue") |> ExAws.request()
+    {:ok, %{body: %{queue_url: queue_url}}} = SQS.create_queue("test_queue_4") |> ExAws.request()
 
-    assert {:ok, _} = SQS.delete_queue(queue_url) |> ExAws.request()
+    assert {:ok, %{body: %{request_id: _}}} = SQS.delete_queue(queue_url) |> ExAws.request()
   end
 
   test "get_queue_attributes/1", context do
-    assert {:ok, _} = SQS.get_queue_attributes(context.queue_url) |> ExAws.request()
+    assert {:ok, %{body: %{attributes: _, request_id: _}}} =
+             SQS.get_queue_attributes(context.queue_url) |> ExAws.request()
   end
 
   test "get_queue_attributes/2", context do
-    assert {:ok, _} =
+    assert {:ok, %{body: %{attributes: _, request_id: _}}} =
              SQS.get_queue_attributes(context.queue_url, [
                :visibility_timeout,
                :message_retention_period
@@ -145,12 +150,17 @@ defmodule ExAws.SQSIntegrationTest do
   end
 
   test "get_queue_url/1" do
-    assert {:ok, %{body: %{queue_url: _}}} = SQS.get_queue_url(@queue_name) |> ExAws.request()
+    assert {:ok, %{body: %{queue_url: queue_url}}} =
+             SQS.get_queue_url(@queue_name) |> ExAws.request()
+
+    assert queue_url |> String.contains?(@queue_name)
   end
 
   test "get_queue_url/2" do
-    assert {:ok, %{body: %{queue_url: _}}} =
+    assert {:ok, %{body: %{queue_url: queue_url}}} =
              SQS.get_queue_url(@queue_name, queue_owner_aws_account_id: "foo") |> ExAws.request()
+
+    assert queue_url |> String.contains?(@queue_name)
   end
 
   @tag :skip
@@ -159,32 +169,41 @@ defmodule ExAws.SQSIntegrationTest do
   end
 
   test "list_queues/0" do
-    assert {:ok, %{body: %{queues: _}}} = SQS.list_queues() |> ExAws.request()
+    assert {:ok, %{body: %{queues: queues}}} = SQS.list_queues() |> ExAws.request()
+    assert is_list(queues)
   end
 
   test "list_queues/1" do
-    assert {:ok, %{body: %{queues: _}}} =
+    assert {:ok, %{body: %{queues: queues}}} =
              SQS.list_queues(queue_name_prefix: "prefix") |> ExAws.request()
+
+    assert is_list(queues)
   end
 
   @tag :skip
   test "list_queue_tags NOT IMPLEMENTED"
 
   test "purge_queue/1", context do
-    assert {:ok, _} = SQS.purge_queue(context.queue_url) |> ExAws.request()
+    assert {:ok, %{body: %{request_id: _}}} =
+             SQS.purge_queue(context.queue_url) |> ExAws.request()
   end
 
   test "receive_message/1", context do
-    assert {:ok, _} = SQS.receive_message(context.queue_url) |> ExAws.request()
+    assert {:ok, %{body: %{messages: messages}}} =
+             SQS.receive_message(context.queue_url) |> ExAws.request()
+
+    assert is_list(messages)
   end
 
   test "receive_message/2", context do
-    assert {:ok, _} =
+    assert {:ok, %{body: %{messages: messages}}} =
              SQS.receive_message(context.queue_url,
                attribute_names: :all,
                max_number_of_messages: 5
              )
              |> ExAws.request()
+
+    assert is_list(messages)
   end
 
   @tag :skip
@@ -196,11 +215,27 @@ defmodule ExAws.SQSIntegrationTest do
   end
 
   test "send_message/2", context do
-    assert {:ok, _} = SQS.send_message(context.queue_url, "lorem ipsum") |> ExAws.request()
+    assert {:ok,
+            %{
+              body: %{
+                md5_of_message_attributes: _,
+                md5_of_message_body: _,
+                message_id: _,
+                request_id: _
+              }
+            }} = SQS.send_message(context.queue_url, "lorem ipsum") |> ExAws.request()
   end
 
   test "send_message/3", context do
-    assert {:ok, _} =
+    assert {:ok,
+            %{
+              body: %{
+                md5_of_message_attributes: _,
+                md5_of_message_body: _,
+                message_id: _,
+                request_id: _
+              }
+            }} =
              SQS.send_message(
                context.queue_url,
                "This is the message body.",
@@ -217,44 +252,47 @@ defmodule ExAws.SQSIntegrationTest do
   end
 
   test "send_message_batch/2", context do
-    assert {:ok, _} =
-             SQS.send_message_batch(
-               context.queue_url,
-               [
-                 [
-                   id: "test_message_1",
-                   message_body: "This is the message body.",
-                   delay_seconds: 30,
-                   message_attributes: [
-                     %{
-                       name: "TestStringAttribute",
-                       data_type: :string,
-                       value: "testing!"
-                     }
-                   ]
-                 ],
-                 [
-                   id: "test_message_2",
-                   message_body: "This is the second message body.",
-                   message_attributes: [
-                     %{
-                       name: "TestAnotherStringAttribute",
-                       data_type: :string,
-                       value: "still testing!"
-                     }
-                   ]
-                 ]
-               ]
-             )
-             |> ExAws.request()
+    {:ok, %{body: %{failures: _, request_id: _, successes: successes}}} =
+      SQS.send_message_batch(
+        context.queue_url,
+        [
+          [
+            id: "test_message_1",
+            message_body: "This is the message body.",
+            delay_seconds: 30,
+            message_attributes: [
+              %{
+                name: "TestStringAttribute",
+                data_type: :string,
+                value: "testing!"
+              }
+            ]
+          ],
+          [
+            id: "test_message_2",
+            message_body: "This is the second message body.",
+            message_attributes: [
+              %{
+                name: "TestAnotherStringAttribute",
+                data_type: :string,
+                value: "still testing!"
+              }
+            ]
+          ]
+        ]
+      )
+      |> ExAws.request()
+
+    assert ["test_message_1", "test_message_2"] == successes |> Enum.map(& &1.id) |> Enum.sort()
   end
 
   test "set_queue_attributes/1", context do
-    assert {:ok, _} = SQS.set_queue_attributes(context.queue_url) |> ExAws.request()
+    assert {:ok, %{body: %{request_id: _}}} =
+             SQS.set_queue_attributes(context.queue_url) |> ExAws.request()
   end
 
   test "set_queue_attributes/2", context do
-    assert {:ok, _} =
+    assert {:ok, %{body: %{request_id: _}}} =
              SQS.set_queue_attributes(context.queue_url, visibility_timeout: 10)
              |> ExAws.request()
   end
