0ffc966de9143cf439811a69b264a054fb75ae8d
Bernard Duggan <bernard@circl.es>
2022-11-28 10:25:16 +1100

    Bump minimum Elixir version to 1.10

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule ExAws.SQS.Mixfile do
       app: :ex_aws_sqs,
       name: "ExAws.SQS",
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
