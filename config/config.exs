67587275d0d699bd6447fbc2be2eeb2ff0d499df
Rodney Norris <rodney@tattdcodemonkey.com>
2023-01-03 10:06:17 -0600

    fix: update to Config module import

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
